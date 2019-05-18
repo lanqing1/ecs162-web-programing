1c3e168bef03dafa08b3866cac7d16761e7ec386
lanqing1 <48611043+lanqing1@users.noreply.github.com>
2019-05-18 01:57:11 -0700

    Update LangoServer.js

--- LangoServer.js
+++ LangoServer.js
@@ -4,12 +4,39 @@ const APIrequest = require('request');
 const http = require('http');
 const APIkey = "AIzaSyDmEaUmKwepuvcQM_T3vijxo02qBzrp1oU";
 const url = "https://translation.googleapis.com/language/translate/v2?key="+APIkey
-// can translate and put data into res
-//need to add storeHandler
+const cmdStr = 'INSERT into Flashcards (user, english,korean, seen, correct) VALUES (1, @0, @1, 0, 0)';
+const sqlite3 = require("sqlite3").verbose();
+const dbFileName = "Flashcards.db";
+const db = new sqlite3.Database(dbFileName);
+
+const cmdStrCreate = 'CREATE TABLE Flashcards (user INT,english TEXT, korean TEXT, seen INT, correct INT)';
+db.run(cmdStrCreate,tableCreationCallback);
+function tableCreationCallback(err) {
+  if (err) {
+    console.log("Table creation error",err);
+  } else {
+    console.log("Database created");
+    // when to close db if not here?
+  }
+}
+function saveHandler(req,res,next){
+  let en = req.query.english;
+  let ko = req.query.korean;
+  db.run(cmdStr, en, ko, insertCallback);
+  //will have duplicated translation in db if click on save mulitiple times
+  function insertCallback(err) {
+    if (err) { console.log(err);}
+  }
+  //printout database
+  db.all ( 'SELECT * FROM flashcards', dataCallback);
+  function dataCallback( err, data ) {console.log(data)}
+  res.statusCode = 204;
+  res.send();
+}
 function translateHandler(req, res, next) {
   let word = req.query.word;
   console.log("english: "+word);
-  let requestObject = { "source": "en", "target": "zh-CN", "q": [word]};
+  let requestObject = { "source": "en", "target": "ko", "q": [word]};
   if(word!=undefined){
     function APIcallback(err, APIresHead, APIresBody) {
       if ((err) || (APIresHead.statusCode != 200)) {
@@ -23,8 +50,8 @@ function translateHandler(req, res, next) {
         if (APIresHead.error) {
           console.log(APIresHead.error);
         } else {
-          res.json({"English":word,
-          "Chinese":APIresBody.data.translations[0].translatedText});
+          kor = APIresBody.data.translations[0].translatedText;
+          res.json({"English":word,"Korean":kor});
         }
       }
     }
@@ -48,10 +75,11 @@ function translateHandler(req, res, next) {
     res.send('Cannot find '+url);
   }
 
-//may need to change app.get(..)
+  // put together the server pipeline
   const app = express()
   app.use(express.static('public'));
-  app.get('/query', translateHandler );
+  app.get('/translate', translateHandler );
+  app.get('/store',saveHandler);
   app.use( fileNotFound );
 
   app.listen(port, function (){console.log('Listening...');} )
