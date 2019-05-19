6bdcd84b534fdcf000f969432ef13a8d6a132c75
lanqing1 <48611043+lanqing1@users.noreply.github.com>
2019-05-18 20:16:50 -0700

    Add files via upload

---
+++ public/lango.js
@@ -0,0 +1,92 @@
+'use strict';
+
+// An element to go into the DOM
+
+var lango = React.createElement(
+	"h1",
+	{ id: "logo" },
+	"Lango!"
+);
+
+function SaveBtn() {
+  return React.createElement(
+		"p",
+		{id: "store",onClick: storeToDB},
+		"save");
+}
+
+function FirstInputCard() {
+  return React.createElement("div",
+	{className: "inputDiv" },
+	React.createElement("textarea",{id: "textArea",onKeyDown: checkReturn}));
+}
+
+
+function FirstCard() {
+  return React.createElement(
+		"div",
+		{className: "outputDiv"},
+		React.createElement("p", {id: "outputGoesHere"}));
+}
+
+var main = React.createElement(
+	"main",
+	null,
+	lango,
+	React.createElement(FirstInputCard, null),
+  React.createElement(SaveBtn, null),
+  React.createElement(FirstCard, null)
+);
+
+ReactDOM.render(main, document.getElementById('root'));
+var object;
+var input;
+
+function checkReturn(event) {
+	if (event.keyCode==13) {
+		 	input = document.getElementById("textArea").value;
+      makeRequest(input);
+    }
+}
+
+function storeToDB() {
+	makeRequestStore(input);
+}
+
+function request(method, url) {
+	let xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	return xhr;
+}
+function makeRequestStore(anything){
+	let url="store?english="+anything+"&korean="+object.Korean;
+	let xhr = request('GET', url);
+	xhr.onload = function() {
+		//xhr.status can check if it's stored.
+		if(xhr.status==204){
+			console.log("successfully stored");
+		}
+	}
+	xhr.send();
+	xhr.onerror = function() {
+		alert('Error: Unable to make request');
+	};
+}
+
+function makeRequest(anything) {
+	let url="translate?word="+anything;
+	let xhr = request('GET', url);
+
+	xhr.onload = function() {
+		let responseStr = xhr.responseText;
+		object = JSON.parse(responseStr);
+		console.log(JSON.stringify(object, undefined, 4));
+		document.getElementById("outputGoesHere").textContent = object.Korean;
+		//xhr.status can check if it's stored.
+	}
+
+	xhr.send();
+	xhr.onerror = function() {
+		alert('Error: Unable to make request');
+	};
+}
---
+++ public/lango.jsx
@@ -0,0 +1,88 @@
+'use strict';
+// An element to go into the DOM
+const lango = <h1 id="logo">Lango!</h1>;
+
+
+
+function SaveBtn(){
+	return (<p id="store" onclick="storeToDB()">save</p>);
+}
+
+function FirstInputCard() {
+	return (<div className="inputDiv">
+	<textarea onkeydown={getInput(this)}/>
+	</div>);
+}
+
+function FirstCard() {
+	return (<div id="outputDiv">
+	<p id="outputGoesHere">Hello, world!</p>
+	</div>);
+}
+
+const main = (<main>
+	{lango}
+	<FirstInputCard/>
+	<SaveBtn/>
+	<FirstCard />
+	</main>
+);
+
+ReactDOM.render(
+	main,
+	document.getElementById('root')
+);
+
+//make request
+console.log("Script Running...");
+var object;
+var input;
+
+function getInput(ele) {
+	if(event.key === 'Enter') {
+		input =ele.value;
+		makeRequest(input,"translate");
+	}
+}
+
+function storeToDB() {
+	makeRequestStore(input);
+}
+
+function request(method, url) {
+	let xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	return xhr;
+}
+function makeRequestStore(anything){
+	let url="store?english="+anything+"&korean="+object.Korean;
+	let xhr = request('GET', url);
+	xhr.onload = function() {
+		//xhr.status can check if it's stored.
+		if(xhr.status==204){
+			console.log("successfully stored");
+		}
+	}
+	xhr.send();
+	xhr.onerror = function() {
+		alert('Error: Unable to make request');
+	};
+}
+
+function makeRequest(anything) {
+	let url="translate?word="+anything;
+	let xhr = request('GET', url);
+
+	xhr.onload = function() {
+		let responseStr = xhr.responseText;
+		object = JSON.parse(responseStr);
+		console.log(JSON.stringify(object, undefined, 4));
+		document.getElementById("outputGoesHere").textContent = object.Korean;
+		//xhr.status can check if it's stored.
+	}
+
+	xhr.send();
+	xhr.onerror = function() {
+		alert('Error: Unable to make request');
+	};
+}
