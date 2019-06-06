c0fdaa78d7ec7aa6576db5cabd561351fe6460f2
Shih-Chieh Hsiao <sjhsiao@ucdavis.edu>
2019-06-06 09:45:14 -0700

    Revert "Update lango.js"
    This reverts commit 2cef411b527b297a9b930927cb3ead6eb1bf5438.

--- user/lango.js
+++ user/lango.js
@@ -234,15 +234,10 @@ function getScore(){
     return(randomCard);
 }
 function nextCard(){
-    let numCards = data.length;
-    if( numCards == 0 ){
-        document.getElementById("reviewOutput").textContent = "Finished Reviewing!";
-    } else {
-        index=getScore();
-        document.getElementById("reviewOutput").textContent = data[index].korean;
-        updateSeenRequest(data[index].english);
-    }
     unFlipCard();
+    index=getScore();
+    document.getElementById("reviewOutput").textContent = data[index].korean;
+    updateSeenRequest(data[index].english);
 }
 function updateSeenRequest(en){
     let url = "/user/seen?english="+en;
